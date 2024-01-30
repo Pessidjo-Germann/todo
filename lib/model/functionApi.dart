09eb90ff565721a71cf2597dd0770f165bf03272
latioms <73307766+latioms@users.noreply.github.com>
2024-01-30 23:45:56 +0100

    Add debugShowCheckedModeBanner: false and fix URI in functionApi.dart

--- lib/model/functionApi.dart
+++ lib/model/functionApi.dart
@@ -1,3 +1,4 @@
+//functionApi.dart
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -5,8 +6,10 @@ import 'package:http/http.dart' as http;
 import 'package:todolist/model/todo.dart';
 
 Future<void> postTodo(List<Todo> list) async {
-  final uri = Uri.parse("http//:127.0.0.1:3000");
+
+  final uri = Uri.parse("http://127.0.0.1:3000");
   Map<String, List<Todo>> request = {"list": list};
+
   try {
     final response = await http.post(uri, body: request);
     if (response.statusCode == 200) {
@@ -21,12 +24,16 @@ Future<void> postTodo(List<Todo> list) async {
 
 Future<List<Todo>> receiveTodo() async {
   final uri = Uri.parse("uri");
+
   try {
+
     final response = await http.post(uri);
+
     if (response.statusCode == 200) {
       Map<List<String>, dynamic> data = jsonDecode(response.body);
       return data[''];
-    } else {
+    }
+    else {
       throw Exception('Erreur lors de l envoie du get');
     }
   } catch (e) {
